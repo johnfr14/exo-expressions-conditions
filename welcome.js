8e20e2bd39ffe7cfd5a6f3bc1fd59e5940a86391
johnfr14 <jonathan.alyra@gmail.com>
2021-02-26 10:17:50 +0100

    comiite

--- welcome.js
+++ welcome.js
@@ -2,5 +2,5 @@ let user = ['Bob', 'Alice']
 let gender = ['male', 'female']
 
 for(let i = 0; i < user.length; i++){
-  gender[i] = 'female' ? console.log(`Bonjour ${user[i]}, vous êtes entrée sur le chan\n`) : console.log(`Bonjour ${user[i]}, vous êtes entré sur le chan\n`)
+  gender[i] === 'female' ? console.log(`Bonjour ${user[i]}, vous êtes entrée sur le chan\n`) : console.log(`Bonjour ${user[i]}, vous êtes entré sur le chan\n`)
 }
